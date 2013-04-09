d662a47dffbad7156e655ae53dcf4251e864c8be
madisonmay13 <madison.may@students.olin.edu>
2013-04-09 15:36:18 -0400

    Timer finished (as far as I can tell).

--- scripts/background.js
+++ scripts/background.js
@@ -5,10 +5,56 @@
 window.addEventListener("load", windowLoaded, false);
 
 function windowLoaded() {
-    var newURL = "http://take5.herokuapp.com";
-    chrome.tabs.create({ url: newURL });
 }
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.browserAction.setIcon({path: 'icons/24_green.png'})
+var min = 0;
+var max = 1;
+var current = max;
+
+var timer = 0;
+var break_timer = 0;
+
+// Reset time
+function toggle() {
+
+  function reset_timer() {
+    clearInterval(break_timer);
+    timer=setInterval(function(){take_a_break()}, 1500000);
+    console.log("Next break started")
+  }
+
+  if (current == 0) {
+
+    clearInterval(timer);
+
+    add = "";
+
+  } else {
+
+    timer=setInterval(function(){take_a_break()}, 1500000);
+    console.log("Timer created");
+
+    function take_a_break(){
+        var newURL = "http://take5.herokuapp.com";
+        chrome.tabs.create({ url: newURL });
+        clearInterval(timer);
+
+        break_timer=setInterval(function(){reset_timer()}, 300000);
+        console.log("Break started.");
+    }
+
+    add = "_1";
+  }
+
+  chrome.browserAction.setIcon({path:"../icons/24" + add + ".png"});
+  // chrome.tabs.executeScript(null, {file: "content" + current + ".js
+  console.log("24" + add + ".png")
+  current++;
+
+  if (current > max)
+    current = min;
+}
+
+chrome.browserAction.onClicked.addListener(function() {
+    toggle();
 });
