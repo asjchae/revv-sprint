92d1dd68040f8f863d2dc951fcd9029d447dc71d
achae <achae@ubuntu.ubuntu-domain>
2013-04-08 21:36:07 -0400

    Working on OAuth, this will not work

--- scripts/background.js
+++ scripts/background.js
@@ -12,3 +12,13 @@ function windowLoaded() {
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.browserAction.setIcon({path: 'icons/24_green.png'})
 });
+
+var oauth = ChromeExOAuth.initBackgroundPage({
+  'request_url': <OAuth request URL>,
+  'authorize_url': <OAuth authorize URL>,
+  'access_url': <OAuth access token URL>,
+  'consumer_key': <OAuth consumer key>,
+  'consumer_secret': <OAuth consumer secret>,
+  'scope': <scope of data access, not used by all OAuth providers>,
+  'app_name': <application name, not used by all OAuth providers>
+});
